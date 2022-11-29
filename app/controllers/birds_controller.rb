45994d4a66b6ca1b27836a4421cb9d7d004bece8
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-29 04:28:59 +0300

    Getting Birds with Id

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,4 +4,17 @@ class BirdsController < ApplicationController
         birds = Bird.all
         render json: birds
     end
-end
+
+    #GET /birds/:id
+    def show
+        bird = Bird.find_by(id: params[:id])
+        #check condition with if else 
+        if bird
+            render json: bird
+        else
+            render json: {error: "Bird not found"}, status: :not_found
+        end
+    end
+    end
+
+
