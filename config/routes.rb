f0d1dc3d1562b0ba97a1cb5f4ee5bd873a83f5d1
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-29 04:09:44 +0300

    using rails resources, that enforces RESTFUL conventions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #Creating routes for index and show action 
-get '/birds', to: 'birds#index'
-get '/birds/:id', to: 'birds#show'
+# get '/birds', to: 'birds#index'
+# get '/birds/:id', to: 'birds#show'
+
+#using rails resources, that enforces RESFUL conventions 
+  resources :birds
 end
