48f50715a0f78811fa8f7425d162fe81e4958535
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-29 04:00:02 +0300

    Creating routes for index and show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Creating routes for index and show action 
+get '/birds', to: 'birds#index'
+get '/birds/:id', to: 'birds#show'
 end
